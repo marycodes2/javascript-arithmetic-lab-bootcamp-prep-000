35da16a353666af01229de2763551b09d9e06469
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-05-29 20:03:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ function add(a, b)
 add(a, b)
 
 function subtract(a, b) {
+<<<<<<< HEAD
 a - b
+=======
+60 - 40
+>>>>>>> 1db4d95bd803e0b6d58f4b15067250cf3752238e
 }
 
 subtract(a, b)
 
 function multiply(a, b) {
+<<<<<<< HEAD
 a * b
+=======
+2 * 3.4
+>>>>>>> 1db4d95bd803e0b6d58f4b15067250cf3752238e
 }
 
 multiply(a, b)
 
 function divide(a, b) {
+<<<<<<< HEAD
 a / b
+=======
+5.0 / 2.5
+>>>>>>> 1db4d95bd803e0b6d58f4b15067250cf3752238e
 }
 
 divide(a, b)
